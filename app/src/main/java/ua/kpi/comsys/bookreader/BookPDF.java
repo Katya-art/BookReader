89dd8184c7d2b4453b0a053690f9c968359ed604
Katerina Kravchenko <katerinakravchenko@Katerinas-MacBook-Pro.local>
2021-06-03 23:40:29 +0300

    Update PDF format

--- app/src/main/java/ua/kpi/comsys/bookreader/BookPDF.java
+++ app/src/main/java/ua/kpi/comsys/bookreader/BookPDF.java
@@ -1,11 +1,16 @@
 package ua.kpi.comsys.bookreader;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -17,16 +22,23 @@ import java.util.Objects;
 public class BookPDF extends AppCompatActivity {
 
     PDFView bookPDFView;
+    ActionBar actionBar;
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_pdf);
         Objects.requireNonNull(this.getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         bookPDFView = findViewById(R.id.pdfView);
+        actionBar = getSupportActionBar();
         Bundle arguments = getIntent().getExtras();
         String path = arguments.get("path").toString();
-        bookPDFView.fromFile(new File(path)).load();
+        bookPDFView.fromFile(new File(path))
+                .nightMode(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+                .load();
+        //bookPDFView.setNightMode(true);
+        //bookPDFView.canScrollHorizontally(bookPDFView.getLayoutDirection());
     }
 
     @Override
@@ -49,9 +61,18 @@ public class BookPDF extends AppCompatActivity {
         if (id == R.id.night_mode) {
             if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#6200EE")));
+                bookPDFView.setNightMode(false);
+                bookPDFView.requestLayout();
+                item.setIcon(R.drawable.mode_night_white_24dp);
                 return true;
             }
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            System.out.println("TEST " + AppCompatDelegate.MODE_NIGHT_YES);
+            item.setIcon(R.drawable.light_mode_white_24dp);
+            actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#121212")));
+            bookPDFView.setNightMode(true);
+            bookPDFView.requestLayout();
             return true;
         }
         //MainWindow.closeLoadingDialog();
